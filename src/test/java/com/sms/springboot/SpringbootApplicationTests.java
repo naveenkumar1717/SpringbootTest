35e0ba76c1fa666d820bda2f9acb2ca7d4da691f
notsen2000 <notsen2000@gmail.com>
2023-12-07 10:22:32 +0530

    Update SpringbootApplicationTests.java

--- src/test/java/com/sms/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/sms/springboot/SpringbootApplicationTests.java
@@ -9,45 +9,7 @@ class SpringbootApplicationTests {
 	@Test
 	void contextLoads() {
 
-		sec 1:
-
-		 14+ years of experience in Design, Development Enhancement & Documenting, Enterprise applications  using JAVA, J2EE technologies.
-Involved in various activities like Design, Coding, End-to-End Testing , Production deployment, and Support for various online applications
-Having ability to learn and adapt quickly with new tools and technology.
-Demonstrated ability to multitask and manage multiple projects in a cross-functional environment
-Excellent communication, interpersonal and presentation skills. Self-starter, customer focused,        detail oriented.
-
-
-
-	Sec 2:
-
-	
-Senthil‘s core expertise covers:
-Domain – Banking( Deposits)
-Extensive knowledge on Java/J2EE , java SE Batch, Spring, Spring Boot ,Microservices, Hibernate, Web Services ,Maven, GitLab, CICD,Gradle,Openshift,Kibana,Docker,Kafka,Oracle,MS SQL Server, jQuery, Agile, Junit,Powermockito,JavaScript,HTML5 and CSS
-Participation in all scrum meetings, PI Plannings, and reporting sprint level deliverables.
-Good in problem solving and troubleshooting skills using core java, spring,springboot , control-M and batch script.
-Manage project specifications, stories,Impedements and bugs in Jira.
-
-
-
-
-
-
-
-	Sec3:
-USAA Bank Deposits: Overall  4+ years of experience in USAA Deposits ART.
-Disputes-Jaguars: 
-	Worked in talon applications which consumes the events from Kafka topic and store that  into Reporting DB tables. Involved in Db table design, data modeler approval, design review discussion , Service Account creation for Sandbox, Test & PROD. And Successfully moved the table from Sandbox to Test and From Test to Prod environments.
- 	Effectively participated in Kafka consumer  implementation ,MR follow-up, Review comments fix. Successfully completed the vulnerability fixes which occur while MR Creation. Provide support on  end-end  Testing, Tag creation, CR Creation and follow-up , Production deployment, Postproduction log verification, Jacoco Code Coverage and Business Demo with Business. Involved in all the story planning & Capacity planning meetings and successfully manage the team concerns on need basis.
-
-AAN : Worked in Existing java Batch Migration, Validation and Monitoring. Batches- DB Updates, File generation, FTP, File Watches, Archive & Purge Jobs. Involved in all the Production related activities like CR creation and follow up ,Coding task approval, Smartbear creation, etc.
-Tech Transformers: Worked in Accessibility module implementation. Shows 100% responsibility in  Code development, Team management ,Status call update, Hub Call update, Metrics report and Value add activity.
-TDMSS 2.0 : Worked in Attached Deposit Account Module Migration. Migrating existing classic approach into new web implementation. Involved In API Design ,Wiki Documentation, etc.
-
-
-
-
+		 
 
 
 
